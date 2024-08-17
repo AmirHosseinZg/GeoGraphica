9428fbed1e7e9eb49f03872f5e5f23ffa043ab53
AmirHosseinZg <zakeri863@gmail.com>
2024-08-17 12:40:37 +0330

    Add colorbar limit specification, colormap selection, and resizable widgets
    - Introduced user-defined colorbar limits for greater control over data
      visualization.
    - Implemented an option menu for selecting the desired colormap,
      enhancing customization.
    - Added resizable functionality to widgets, improving user interface
      flexibility.
      and also a image added for the application background

--- GeoGraphicaPr/Cos(x)+Cos(y)_plot.py
+++ GeoGraphicaPr/Cos(x)+Cos(y)_plot.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import PhotoImage
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -26,7 +27,7 @@ def plot_graph():
         the area in which the graph is drawn """
 
         # The following line of code creates a filled contour plot
-        contour_filled = ax.contourf(X, Y, Z, levels=int(contours.get()), cmap='viridis')
+        contour_filled = ax.contourf(X, Y, Z, levels=int(contours.get()), cmap=selected_colormap.get())
         """cmap='viridis': the colormap used to fill the areas between the contour lines. viridis is one of matplotlib's
          default options, which has a yellow-green-blue color spectrum."""
 
@@ -37,8 +38,9 @@ def plot_graph():
         Output: This function creates a QuadContourSet object named contour_lines that represents the contour lines."""
 
         # Add a colorbar to show the mapping of values to colors
-        fig.colorbar(contour_filled, ax=ax, label='Function Value')
-
+        colorbar = fig.colorbar(contour_filled, ax=ax, label='Function Value')
+        # colorbar limits
+        colorbar.set_ticks([float(Colorbar_lower_bound.get()), float(Colorbar_upper_bound.get())])
         # Set labels and title
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
@@ -62,33 +64,68 @@ window.title("Cos(x) + Cos(y) Plotter")
 frame = ttk.Frame(window, padding="100")
 frame.pack(fill=tk.BOTH, expand=True)
 
+# # GUI background
+# image = PhotoImage(file="H:\Zakeri\Samadi_pr\GeoGraphica\Sources\\test_plot_darkmod.png")
+# ttk.Label(frame, image=image).place(x=0, y=0)
+
 ttk.Label(frame, text="Enter x range of changes :  from").grid(row=0, column=0, padx=5, pady=5)
 x_lower_bound = ttk.Entry(frame)
-x_lower_bound.grid(row=0, column=1, padx=5, pady=5)
+x_lower_bound.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
 
 ttk.Label(frame, text="to ").grid(row=0, column=2, padx=5, pady=5)
 x_upper_bound = ttk.Entry(frame)
-x_upper_bound.grid(row=0, column=3, padx=5, pady=5)
+x_upper_bound.grid(row=0, column=3, padx=5, pady=5, sticky="ew")
 
 ttk.Label(frame, text="Enter y range of changes :  from ").grid(row=1, column=0, padx=5, pady=5)
 y_lower_bound = ttk.Entry(frame)
-y_lower_bound.grid(row=1, column=1, padx=5, pady=5)
+y_lower_bound.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
 
 ttk.Label(frame, text="to ").grid(row=1, column=2, padx=5, pady=5)
 y_upper_bound = ttk.Entry(frame)
-y_upper_bound.grid(row=1, column=3, padx=5, pady=5)
+y_upper_bound.grid(row=1, column=3, padx=5, pady=5, sticky="ew")
 
 ttk.Label(frame, text="Enter number of contours :          ").grid(row=2, column=0, padx=5, pady=5)
 contours = ttk.Entry(frame)
-contours.grid(row=2, column=1, padx=5, pady=5)
+contours.grid(row=2, column=1, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text="Enter Colorbar range of changes :  from ").grid(row=3, column=0, padx=5, pady=5)
+Colorbar_lower_bound = ttk.Entry(frame)
+Colorbar_lower_bound.grid(row=3, column=1, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text="to ").grid(row=3, column=2, padx=5, pady=5)
+Colorbar_upper_bound = ttk.Entry(frame)
+Colorbar_upper_bound.grid(row=3, column=3, padx=5, pady=5, sticky="ew")
+
+# Create a list of colormaps available in matplotlib
+colormaps = plt.colormaps()
+# A variable to hold the selected color map
+selected_colormap = tk.StringVar(window)
+# set the default value for selected_colormap
+selected_colormap.set(colormaps[0])
+# Create a selection menu
+ttk.OptionMenu(frame, selected_colormap, *colormaps).grid(row=4, column=0, padx=5, pady=5, sticky="ew")
 
 # Create plot button
 plot_button = ttk.Button(frame, text="Plot", command=plot_graph)
-plot_button.grid(row=3, column=0, pady=5, columnspan=2)
+plot_button.grid(row=5, column=1, pady=5, columnspan=2)
 
 # Create error label
 error_label = ttk.Label(frame, text="", foreground="red")
-error_label.grid(row=4, column=0, columnspan=2, pady=5)
+error_label.grid(row=6, column=0, columnspan=4, pady=5)
+
+# Configure rows and columns to be resizable
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_rowconfigure(1, weight=1)
+frame.grid_rowconfigure(2, weight=1)
+frame.grid_rowconfigure(3, weight=1)
+frame.grid_rowconfigure(4, weight=1)
+frame.grid_rowconfigure(5, weight=1)
+frame.grid_rowconfigure(6, weight=1)
+
+frame.grid_columnconfigure(0, weight=1)
+frame.grid_columnconfigure(1, weight=1)
+frame.grid_columnconfigure(2, weight=1)
+frame.grid_columnconfigure(3, weight=1)
 
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
