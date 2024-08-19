f6e8b9f634fe9d57054984ecaef6a7a52880e36c
AmirHosseinZg <zakeri863@gmail.com>
2024-08-19 13:23:38 +0330

    Constant class created for store the constant variables - Txx GUI implemented

---
+++ GeoGraphicaPr/Txx_Plotter/Constant.py
@@ -0,0 +1,19 @@
+class Constants:
+    def __init__(self):
+        self.__EOTVOS = pow(10, -9)
+        self.__GM = 3.986004418 * pow(10, 14)
+        self.__A = 6378137.0
+        self.__Nmax = 360
+
+    @property
+    def EOTVOS(self):
+        return self.__EOTVOS
+
+    def Gm(self):
+        return self.__GM
+
+    def A(self):
+        return self.__A
+
+    def Nmax(self):
+        return self.__Nmax
---
+++ GeoGraphicaPr/Txx_Plotter/Txx.py
@@ -0,0 +1,155 @@
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import Constant
+
+
+def on_closing():
+    window.destroy()  # this will close the tkinter window
+    exit()  # this will terminate the program
+
+
+def sigma_c(n, m):
+    return
+    # TODO : implementation
+
+
+def sigma_s(n, m):
+    return
+    # TODO : implementation
+
+
+def Txx(r, phi, landa):
+    constants = Constant.Constants()
+    part_one = (1 / constants.EOTVOS) * (constants.Gm() / pow(constants.A(), 3))
+
+    part_two = 0
+    for n in range(2, constants.Nmax() + 1):
+        for m in range(0, n + 1):
+            part_two += pow((constants.A() / r), n + 3) * (
+                        (sigma_c(n, m) * np.cos(m * landa)) + (sigma_s(n, m) * np.sin(m * landa)))
+
+    part_three = None # TODO
+
+def plot_graph():
+    try:
+        # Define the range for x and y
+        phi = np.linspace(int(longitude_lowerbound.get()), int(longitude_upperbound.get()), 1000)
+        landa = np.linspace(int(latitude_lowerbound.get()), int(latitude_upperbound.get()), 1000)
+        phi, landa = np.meshgrid(phi, landa)
+
+        # Calculate the function
+        Z = np.cos(X) + np.cos(Y)
+
+        # Plot the filled contour
+        fig, ax = plt.subplots()
+        """plt.subplot() return an figure object and an axis object . fig represents the overall shape and ax represents
+        the area in which the graph is drawn """
+
+        # The following line of code creates a filled contour plot
+        contour_filled = ax.contourf(X, Y, Z, levels=int(contours.get()), cmap=selected_colormap.get())
+        """cmap='viridis': the colormap used to fill the areas between the contour lines. viridis is one of matplotlib's
+         default options, which has a yellow-green-blue color spectrum."""
+
+        # Add contour lines on top
+        contour_lines = ax.contour(X, Y, Z, levels=int(contours.get()), colors='black', linewidths=0.5)
+        """colors='black': makes the color of contours lines black 
+        linewidths=0.5: specifies the thickness of the contour lines , which here is equal to 0.5 units
+        Output: This function creates a QuadContourSet object named contour_lines that represents the contour lines."""
+
+        # Add a colorbar to show the mapping of values to colors
+        colorbar = fig.colorbar(contour_filled, ax=ax, label='Function Value')
+        # colorbar limits
+        colorbar.set_ticks([float(Colorbar_lower_bound.get()), float(Colorbar_upper_bound.get())])
+        # Set labels and title
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_title(
+            f' ContourPlot [Cos(x) + Cos(y) , (x,{x_lower_bound.get()},{x_upper_bound.get()}) , (y,{y_lower_bound.get()},{y_upper_bound.get()}) , contours -> {contours.get()} ]')
+        ax.grid(True)
+
+        # Display the plot in the Tkinter window
+        canvas = FigureCanvasTkAgg(fig, master=window)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    except ValueError:
+        error_label.config(text="Please enter valid numeric values.")
+
+
+# create the main window
+window = tk.Tk()
+window.title = "Txx Plotter"
+
+# create the input frame
+frame = ttk.Frame(window, padding=100)
+frame.pack(fill=tk.BOTH, expand=True)
+
+ttk.Label(frame, text="Enter the range of longitude changes from : ").grid(row=0, column=0, padx=5, pady=5)
+longitude_lowerbound = ttk.Entry(frame)
+longitude_lowerbound.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text=" To ").grid(row=0, column=2, padx=5, pady=5)
+longitude_upperbound = ttk.Entry(frame)
+longitude_upperbound.grid(row=0, column=3, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text="Enter the range of Latitude changes from : ").grid(row=1, column=0, padx=5, pady=5)
+latitude_lowerbound = ttk.Entry(frame)
+latitude_lowerbound.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text=" To ").grid(row=1, column=2, padx=5, pady=5)
+latitude_upperbound = ttk.Entry(frame)
+latitude_upperbound.grid(row=1, column=3, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text="Enter your desired resolution : ").grid(row=2, column=0, padx=5, pady=5)
+resolution = ttk.Entry(frame)
+resolution.grid(row=2, column=1, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text="Enter number of contours :          ").grid(row=3, column=0, padx=5, pady=5)
+contours = ttk.Entry(frame)
+contours.grid(row=3, column=1, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text="Enter Colorbar range of changes :  from ").grid(row=4, column=0, padx=5, pady=5)
+Colorbar_lower_bound = ttk.Entry(frame)
+Colorbar_lower_bound.grid(row=4, column=1, padx=5, pady=5, sticky="ew")
+
+ttk.Label(frame, text="to ").grid(row=4, column=2, padx=5, pady=5)
+Colorbar_upper_bound = ttk.Entry(frame)
+Colorbar_upper_bound.grid(row=4, column=3, padx=5, pady=5, sticky="ew")
+
+# Create a list of colormaps available in matplotlib
+colormaps = plt.colormaps()
+# A variable to hold the selected color map
+selected_colormap = tk.StringVar(window)
+# set the default value for selected_colormap
+selected_colormap.set(colormaps[0])
+# Create a selection menu
+ttk.OptionMenu(frame, selected_colormap, *colormaps).grid(row=4, column=0, padx=5, pady=5, sticky="ew")
+
+# Create plot button
+plot_button = ttk.Button(frame, text="Plot")
+plot_button.grid(row=5, column=1, pady=5, columnspan=2)
+
+# Create error label
+error_label = ttk.Label(frame, text="", foreground="red")
+error_label.grid(row=6, column=0, columnspan=4, pady=5)
+
+# Configure rows and columns to be resizable
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_rowconfigure(1, weight=1)
+frame.grid_rowconfigure(2, weight=1)
+frame.grid_rowconfigure(3, weight=1)
+frame.grid_rowconfigure(4, weight=1)
+frame.grid_rowconfigure(5, weight=1)
+frame.grid_rowconfigure(6, weight=1)
+
+frame.grid_columnconfigure(0, weight=1)
+frame.grid_columnconfigure(1, weight=1)
+frame.grid_columnconfigure(2, weight=1)
+frame.grid_columnconfigure(3, weight=1)
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+# Start the main loop
+window.mainloop()
