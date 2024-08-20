f2845bcc8bc01ea57dca78f8464ba9f5e61f47ac
AmirHosseinZg <zakeri863@gmail.com>
2024-08-20 13:08:51 +0330

    EGM96_data.py a module to handle the data in a dictionary

---
+++ GeoGraphicaPr/Txx_Plotter/EGM96_data.py
@@ -0,0 +1,33 @@
+import csv
+
+# Initialize the data dictionary
+data = {}
+
+# Specify the path to your CSV file
+csv_file_path = "H:/Zakeri/Samadi_pr/GeoGraphica/Sources/EGM96.csv"
+
+# Open the CSV file
+with open(csv_file_path, mode='r', newline='') as file:
+    # Create a CSV reader object
+    csv_reader = csv.reader(file)
+
+    # Iterate over each row in the CSV file
+    for row in csv_reader:
+        # Skip empty rows
+        if not row:
+            continue
+
+        try:
+            # Extract and convert data
+            n = int(row[0])
+            m = int(row[1])
+            C_nm = row[2]
+            S_nm = row[3]
+
+            # Initialize nested dictionaries if needed
+            if n not in data:
+                data[n] = {}
+            data[n][m] = (C_nm, S_nm)
+
+        except ValueError as e:
+            print(f"Error processing row {row}: {e}")
